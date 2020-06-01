2d23ac53e220ab4c07c08e689750a36fdc4e5900
muhammad riansyah <mriansyah93@gmail.com>
2020-06-01 07:15:15 +0700

    add migration

--- database/migrations/2020_05_31_234426_create_villages_table.php
+++ database/migrations/2020_05_31_234426_create_villages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+class CreateVillagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('villages', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); 
-            $table->string('email'); 
-            $table->string('handphone'); 
-            // cara buat FK di laravel?
-            $table->text('detail_address'); 
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('villages');
     }
 }
