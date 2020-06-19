7d4af6fcbfa167731e70a1a4068c9f7a97a25ef4
muhammad riansyah <mriansyah93@gmail.com>
2020-06-19 14:00:48 +0700

    rename model class

--- app/Contact.php
+++ app/Contact.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class village extends Model
+class Contact extends Model
 {
+    protected $table = 'contact';
+
     //
 }
--- app/Village.php
+++ app/Village.php
@@ -4,9 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class contact extends Model
+class Village extends Model
 {
-    protected $table = 'contact';
-
     //
 }
